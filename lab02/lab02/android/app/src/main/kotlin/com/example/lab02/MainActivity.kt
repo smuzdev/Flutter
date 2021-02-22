6618d7aea33b3d708719eb2a58f63166dd12b64b
smuzdev <smuzdev@yandex.ru>
2021-02-23 00:49:19 +0300

    add: Functions

--- lab02/lab02/android/app/src/main/kotlin/com/example/lab02/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lab02
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
--- lab02/lab02/lib/IComputerGame.dart
+++
@@ -1,9 +0,0 @@
-class IComputerGame {
-  void play() {
-    print("Let's play!");
-  }
-
-  void quit() {
-    print("Quit game :(");
-  }
-}
